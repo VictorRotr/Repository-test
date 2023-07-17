01aa328f4b4b789c01dde8408efdc042f83674b2
Rotaru Victor <victoras.2001@yahoo.com>
2023-07-17 16:44:09 +0300

    buton functional zona 1 si 3

--- main.js
+++ main.js
@@ -1,10 +1,35 @@
+let stiatiCa = [];
+let list = document.getElementById('stiatiCa');
+let outputParagraph = document.getElementById('raspuns');
+
 const inputInfo = () => {
     let result = document.getElementById('inputStiatiCa').value;
     console.log(result);
-    return result;
-}
+    stiatiCa.push(result);
 
-document.getElementById('addBtn').addEventListener("click", inputInfo)
-const astaImiDa = inputInfo();
-const list1 = document.getElementById('li1');
-list1.textContent = astaImiDa;
+list.innerHTML ='';
+
+ stiatiCa.forEach((item) => {
+     let li = document.createElement('li');
+     li.innerText = item;
+     list.appendChild(li);
+});
+}
+console.log(stiatiCa)
+document.getElementById('addBtn').addEventListener("click", inputInfo);
+
+
+const oddEven  =() => {
+    let output;
+    if(stiatiCa.length === 0 && stiatiCa.length % 2 === 0) {
+        output = 'Lista nu contine nici-un element'
+    }else if(stiatiCa.length % 2 === 0){
+         output = 'Lista are un numar par de iteme'
+    }else if(stiatiCa.length % 2 != 0){
+        output = 'Lista are un numar impar de iteme'
+    };
+    
+    console.log(output);
+    outputParagraph.textContent = output;
+};
+document.getElementById('oddEvenBtn').addEventListener('click',oddEven)
